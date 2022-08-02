e136b6194320ff1b9b181748f59a56423b374d16
PregoBS <rodrigocherutti@hotmail.com>
2022-08-02 15:05:40 -0300

    added new tests

--- api/tests/test_metatrader.py
+++ api/tests/test_metatrader.py
@@ -20,21 +20,38 @@ def test_shutdown() -> None:
 def test_create_dataframe_from_bars() -> None:
     assert api.connect() is True
     dataframe = api.create_dataframe_from_bars(symbol, TimeFrame.M5, 0, bars)
+    assert dataframe is not None
     assert len(dataframe) == bars
+    assert api.shutdown() is True
+
+
+def test_do_not_create_dataframe_from_bars() -> None:
+    assert api.connect() is True
+    dataframe = api.create_dataframe_from_bars("invalid-symbol", TimeFrame.M5, 0, bars)
+    assert dataframe is None
     assert api.shutdown() is True
 
 
 def test_create_dataframe_from_date() -> None:
     assert api.connect() is True
-    dataframe = api.create_dataframe_from_date(symbol, TimeFrame.D1, today + timedelta(days=-bars), today)
+    dataframe = api.create_dataframe_from_date(symbol, TimeFrame.D1, today - timedelta(days=bars), today)
+    assert dataframe is not None
     # Weekends or holidays may not have data
     # The dataframe length should be smaller than the timedelta
     assert 15 <= len(dataframe) <= bars
     assert api.shutdown() is True
 
 
+def test_do_not_create_dataframe_from_date() -> None:
+    assert api.connect() is True
+    dataframe = api.create_dataframe_from_date("invalid-symbol", TimeFrame.M5, today - timedelta(days=bars), today)
+    assert dataframe is None
+    assert api.shutdown() is True
+
+
 def test_standardize_dataframe() -> None:
     assert api.connect() is True
     dataframe = api.create_dataframe_from_bars(symbol, TimeFrame.M5, 0, bars)
+    assert dataframe is not None
     assert dataframe.columns.to_list().sort() == api.dataframe_columns.sort()
     assert api.shutdown() is True
