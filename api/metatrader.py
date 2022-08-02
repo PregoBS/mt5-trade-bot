4ded2e66abf0a731d129abe753bfd0956b31a2a1
PregoBS <rodrigocherutti@hotmail.com>
2022-08-02 14:54:58 -0300

    methods updated to use enum timeframes

--- api/metatrader.py
+++ api/metatrader.py
@@ -1,25 +1,58 @@
 from api.market_data_api import MarketDataAPI
 from datetime import datetime, timedelta
+from enum import Enum
 import MetaTrader5 as mt5
 import numpy as np
 import pandas as pd
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+class TimeFrame(Enum):
+    M1 = mt5.TIMEFRAME_M1
+    M5 = mt5.TIMEFRAME_M5
+    M15 = mt5.TIMEFRAME_M15
+    H1 = mt5.TIMEFRAME_H1
+    H4 = mt5.TIMEFRAME_H4
+    D1 = mt5.TIMEFRAME_D1
+    W1 = mt5.TIMEFRAME_W1
+    MN1 = mt5.TIMEFRAME_MN1
 
 
 class MetaTrader5API(MarketDataAPI):
-    def create_dataframe_from_bars(self, symbol: str, timeframe: str, start_position: int, bars: int) -> pd.DataFrame or None:
-        tf = self._switch_timeframe(timeframe)
-        if tf is not None:
-            dataframe = pd.DataFrame(mt5.copy_rates_from_pos(symbol, tf, start_position, bars))
-            if not dataframe.empty:
-                return self._standardize_dataframe(dataframe, symbol)
+    def connect(self) -> bool:
+        """MT5 connection"""
+        kwargs = dict(
+            path=os.getenv("MT5_PATH") or "",
+            login=int(os.getenv("MT5_LOGIN")) or 0,
+            server=os.getenv("MT5_SERVER") or "",
+            password=os.getenv("MT5_PASSWORD") or "",
+        )
+        if not mt5.initialize(**kwargs):
+            print(f"MT5 Initialize Failed, error code = {mt5.last_error()}")
+            return False
+        else:
+            return True
+
+    def shutdown(self) -> bool:
+        """MT5 connection shutdown"""
+        return mt5.shutdown()
+
+    def create_dataframe_from_bars(self, symbol: str, timeframe: TimeFrame, start_position: int,
+                                   bars: int) -> pd.DataFrame or None:
+        dataframe = pd.DataFrame(mt5.copy_rates_from_pos(symbol, timeframe.value, start_position, bars))
+        if not dataframe.empty:
+            return self._standardize_dataframe(dataframe, symbol)
         return None
 
-    def create_dataframe_from_date(self, symbol: str, timeframe: str, start_date: datetime, end_date: datetime) -> pd.DataFrame or None:
-        tf = self._switch_timeframe(timeframe)
-        if tf is not None:
-            dataframe = pd.DataFrame(mt5.copy_rates_range(symbol, tf, start_date, end_date))
-            if not dataframe.empty:
-                return self._standardize_dataframe(dataframe, symbol)
+    def create_dataframe_from_date(self, symbol: str, timeframe: TimeFrame, start_date: datetime,
+                                   end_date: datetime) -> pd.DataFrame or None:
+        dataframe = pd.DataFrame(mt5.copy_rates_range(symbol, timeframe.value, start_date, end_date))
+        if not dataframe.empty:
+            return self._standardize_dataframe(dataframe, symbol)
         return None
 
     def _standardize_dataframe(self, dataframe: pd.DataFrame, symbol: str) -> pd.DataFrame:
@@ -47,16 +80,3 @@ class MetaTrader5API(MarketDataAPI):
         dataframe['Close'] = round(dataframe['Close'], digits)
         del dataframe["Date"]
         return dataframe
-
-    def _switch_timeframe(self, timeframe: str) -> str or int or None:
-        switcher = {
-            "M1": mt5.TIMEFRAME_M1,
-            "M5": mt5.TIMEFRAME_M5,
-            "M15": mt5.TIMEFRAME_M15,
-            "H1": mt5.TIMEFRAME_H1,
-            "H4": mt5.TIMEFRAME_H4,
-            "D1": mt5.TIMEFRAME_D1,
-            "W1": mt5.TIMEFRAME_W1,
-            "MN1": mt5.TIMEFRAME_MN1
-        }
-        return switcher.get(timeframe, None)
