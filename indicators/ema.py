ff5fe13c2f9b0ea10c37f1871ec0468b9a963f4a
PregoBS <rodrigocherutti@hotmail.com>
2022-08-02 16:28:43 -0300

    Adding the EMA indicator

---
+++ indicators/ema.py
@@ -0,0 +1,16 @@
+from indicators.indicator import Indicator
+import pandas as pd
+
+
+class EMA(Indicator):
+    def __init__(self, name: str, period: int) -> None:
+        Indicator.__init__(self, name)
+        self.period = period
+
+    def calculate(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        return self._ema(dataframe)
+
+    def _ema(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        dataframe["EMA" + str(self.period)] = dataframe["Close"].ewm(span=self.period, min_periods=self.period).mean()
+        dataframe = dataframe.fillna(method="bfill")
+        return dataframe
