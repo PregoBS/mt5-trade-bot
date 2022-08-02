6f920c8e2dab401481dba5074378e016c627e310
PregoBS <rodrigocherutti@hotmail.com>
2022-08-02 16:32:34 -0300

    improving tests for EMA indicator

--- indicators/tests/test_ema.py
+++ indicators/tests/test_ema.py
@@ -1,14 +1,12 @@
-from api.market_data_api import MarketDataAPI
-import fixtures
+from .fixtures import btc_dataframe
 from indicators.ema import EMA
-import numpy as np
 from pandas import DataFrame
 import pytest
 
 
 @pytest.fixture
 def dataframe() -> DataFrame:
-    return fixtures.btc_dataframe()
+    return btc_dataframe()
 
 
 def test_calculate(dataframe: DataFrame) -> None:
