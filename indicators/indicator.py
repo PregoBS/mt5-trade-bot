cff6835e4eccdf8ece94544cb2d277a316d25663
PregoBS <rodrigocherutti@hotmail.com>
2022-08-02 15:35:21 -0300

    adding the Indicator abstract class

---
+++ indicators/indicator.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+from pandas import DataFrame
+
+
+class Indicator(ABC):
+    """Calculate an indicator on a pandas DataFrame"""
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def calculate(self, dataframe: DataFrame) -> DataFrame:
+        pass
