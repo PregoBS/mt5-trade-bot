123555962b49a0c2f70aaaeb951559df26aedbb6
PregoBS <rodrigocherutti@hotmail.com>
2022-08-11 15:13:36 -0300

    Added Signal class

---
+++ signals/signal.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from pandas import DataFrame
+
+
+@dataclass
+class SignalObj:
+    name: str
+    symbol: str
+    timeframe: str
+    signal: int
+
+
+class Signal(ABC):
+    def __init__(self, name: str):
+        self.name = name
+
+    @abstractmethod
+    def get_signal(self, symbol: str, timeframe: str, dataframe: DataFrame) -> SignalObj:
+        pass
