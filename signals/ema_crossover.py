47c30b8b94ea846d647da0f4613ece2951915e91
PregoBS <rodrigocherutti@hotmail.com>
2022-08-11 15:27:45 -0300

    Added Signal EMA Crossover

--- signals/ema_crossover.py
+++ signals/ema_crossover.py
@@ -9,10 +9,10 @@ class EMACrossover(Signal):
         self.slow = slow_ema_period
 
     def get_signal(self, symbol: str, timeframe: str, dataframe: DataFrame) -> SignalObj:
-        return self._ema_crossover()
+        return self._ema_crossover(symbol, timeframe, dataframe)
 
     def _ema_crossover(self, symbol: str, timeframe: str, dataframe: DataFrame) -> SignalObj:
-        signal = 
+        signal = dataframe["EMACrossover17_34"].iloc[-1]
         return SignalObj(
             name=self.name,
             symbol=symbol,
