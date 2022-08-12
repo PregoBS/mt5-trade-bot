dcf36bde1d06fe93eb23f96171189718ac78864a
PregoBS <rodrigocherutti@hotmail.com>
2022-08-12 10:33:45 -0300

    Added Strategy Manager

---
+++ strategies/__init__.py
@@ -0,0 +1,3 @@
+from strategies.manager import Manager
+from strategies.EMACrossover import EMACrossover
+from strategies.strategy import Strategy, State
---
+++ strategies/manager.py
@@ -0,0 +1,22 @@
+from design_patterns.observer_pattern import Observer
+from pandas import DataFrame
+from signals.signal import SignalObj
+from strategies.strategy import Strategy
+from typing import List
+
+
+class Manager:
+    def __init__(self) -> None:
+        self.strategies: List[Strategy] = []
+
+    def add(self, strategy: Strategy) -> None:
+        return self.strategies.append(strategy)
+
+    def subscribe_observer(self, observer: Observer) -> None:
+        for strategy in self.strategies:
+            strategy.subscribe(observer)
+        return None
+
+    def verify_all(self, symbol: str, timeframe: str, dataframe: DataFrame, signals: List[SignalObj]) -> None:
+        for strategy in self.strategies:
+            strategy.verify(symbol, timeframe, dataframe, signals)
