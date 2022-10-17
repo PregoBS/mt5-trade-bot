abde56a021585ea5289ff0260f6d26b3702fe07c
PregoBS <rodrigocherutti@hotmail.com>
2022-10-17 18:33:21 -0300

    Refactoring methods

--- strategies/EMACrossover.py
+++ strategies/EMACrossover.py
@@ -1,30 +1,21 @@
 from __future__ import annotations
-from shared_data_structures.structures import StrategySettings, StrategyState, OrderType, OrderExecution
+from shared_data_structures.structures import StrategyState, OrderType, OrderExecution
 from strategies.strategy import Strategy
 from typing import List, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from api import Attributes, MarketDataAPI, Position
+    from api import Attributes, MarketDataAPI, Position, TimeFrames
     from pandas import DataFrame
     from risk_management import TradeRiskManager
     from signals import SignalObj
 
 
 class EMACrossover(Strategy):
-    def __init__(self, name: str, magic_number: int) -> None:
-        super().__init__(name, magic_number)
-        self._is_buy = False
-        self._is_sell = False
-        self._set_strategy_settings(StrategySettings(max_volume=1.0,
-                                                     can_open_multiple_positions=False))
-
-    def check_new_position(self, symbol: str, timeframe: str, dataframe: DataFrame, signals: List[SignalObj]) -> None:
-        self._state = None
-        self._is_buy = False
-        self._is_sell = False
-        self._check_signals(symbol, timeframe, signals)
-        self._change_state_new_position(symbol, timeframe, dataframe)
-        self.notify()
+    def check_new_position(self, symbol: str, timeframe: TimeFrames, dataframe: DataFrame, signals: List[SignalObj]) -> None:
+        self._reset_state()
+        self._check_signals(symbol, timeframe.value, signals)
+        self._change_state_new_position(symbol, timeframe.value, dataframe)
+        self._notify()
 
     def _check_signals(self, symbol: str, timeframe: str, signals: List[SignalObj]) -> None:
         ema_crossover = self._get_specific_signal("EMACrossover", symbol, timeframe, signals)
@@ -42,26 +33,41 @@ class EMACrossover(Strategy):
         return None
 
     def _set_buy_state(self, symbol: str, timeframe: str, dataframe: DataFrame):
-        action = OrderExecution.OPEN_POSITION
-        order_type = OrderType.BUY
-        price_open = dataframe["Close"].iloc[-1]
-        spread = dataframe["Spread"].iloc[-1]
-        digits = dataframe["_Digits"].iloc[-1]
-        stop_loss = dataframe["Low"].iloc[-2] - dataframe["ATR20"].iloc[-1] * 0.5
-        self._set_state(symbol, timeframe, action, order_type, price_open, spread, digits, stop_loss, 0.0, 0.0, 0.0, 0)
-        return None
+        self._state = StrategyState(symbol=symbol,
+                                    timeframe=timeframe,
+                                    strategy=self.name,
+                                    magic=self.magic,
+                                    action=OrderExecution.OPEN_POSITION,
+                                    settings=self._settings,
+                                    order_type=OrderType.BUY,
+                                    price=dataframe["Close"].iloc[-1],
+                                    spread=dataframe["Spread"].iloc[-1],
+                                    digits=dataframe["_Digits"].iloc[-1],
+                                    stop_loss=dataframe["Low"].iloc[-2] - dataframe["ATR20"].iloc[-1] * 0.5,
+                                    stop_gain=0.0,
+                                    limit_price=0.0,
+                                    stop_limit=0.0,
+                                    ticket=0)
 
     def _set_sell_state(self, symbol: str, timeframe: str, dataframe: DataFrame) -> None:
-        action = OrderExecution.OPEN_POSITION
-        order_type = OrderType.SELL
-        price_open = dataframe["Close"].iloc[-1]
-        spread = dataframe["Spread"].iloc[-1]
-        digits = dataframe["_Digits"].iloc[-1]
-        stop_loss = dataframe["High"].iloc[-2] + dataframe["ATR20"].iloc[-1] * 0.5
-        self._set_state(symbol, timeframe, action, order_type, price_open, spread, digits, stop_loss, 0.0, 0.0, 0.0, 0)
-        return None
+        self._state = StrategyState(symbol=symbol,
+                                    timeframe=timeframe,
+                                    strategy=self.name,
+                                    magic=self.magic,
+                                    action=OrderExecution.OPEN_POSITION,
+                                    settings=self._settings,
+                                    order_type=OrderType.SELL,
+                                    price=dataframe["Close"].iloc[-1],
+                                    spread=dataframe["Spread"].iloc[-1],
+                                    digits=dataframe["_Digits"].iloc[-1],
+                                    stop_loss=dataframe["High"].iloc[-2] + dataframe["ATR20"].iloc[-1] * 0.5,
+                                    stop_gain=0.0,
+                                    limit_price=0.0,
+                                    stop_limit=0.0,
+                                    ticket=0)
 
-    def check_protect(self, position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager) -> None:
+    def check_protect(self, position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager, dataframe: DataFrame) -> None:
+        self._reset_state()
         symbol_attr = api.get_symbol_attributes(position.symbol)
         stop_loss = position.price_open
         if position.stop_loss == round(stop_loss, symbol_attr.digits):
@@ -69,85 +75,82 @@ class EMACrossover(Strategy):
         if not self._is_to_protect(position, trade_risk):
             return None
         self._change_state_protect(position, symbol_attr)
-        self.notify()
+        self._notify()
 
     def _is_to_protect(self, position: Position, trade_risk: TradeRiskManager) -> bool:
         settings = trade_risk.risk_settings
 
-        if position.magic != self.magic or position.timeframe != settings.timeframe:
+        if not self._is_the_right_position(position):
             return False
 
+        # BELOW ALL CONDITIONS TO PROTECT THE POSITION
+        # THE ORDER MATTERS. THE LAST TRUE CONDITION WILL BE STORED AS PROTECT_STATE
         if position.profit >= settings.op_goal / 3:
             print(f"{position.symbol} - Lucro de {position.profit:.2f}, protegendo trade!")
             return True
+
         return False
 
-    def _change_state_protect(self, position: Position, symbol_attributes: Attributes) -> None:
-        # TODO Refactor this position type == 0 to use enum class in comparison
-        if position.type == 0:
-            order_type = OrderType.BUY
-        else:
-            order_type = OrderType.SELL
+    def _protection_01(self) -> None:
+        pass
 
-        symbol = symbol_attributes.symbol
-        timeframe = "NA"
-        action = OrderExecution.MODIFY_POSITION
-        spread = symbol_attributes.spread
-        digits = symbol_attributes.digits
-        stop_loss = position.price_open
-        ticket = position.ticket
-        self._set_state(symbol, timeframe, action, order_type, 0.0, spread, digits, stop_loss, 0.0, 0.0, 0.0, ticket)
+    def _change_state_protect(self, position: Position, symbol_attributes: Attributes) -> None:
+        self._state = StrategyState(symbol=symbol_attributes.symbol,
+                                    timeframe="NA",
+                                    strategy=self.name,
+                                    magic=self.magic,
+                                    action=OrderExecution.MODIFY_POSITION,
+                                    settings=self._settings,
+                                    order_type=position.type,
+                                    price=0.0,
+                                    spread=symbol_attributes.spread,
+                                    digits=symbol_attributes.digits,
+                                    stop_loss=position.price_open,
+                                    stop_gain=0.0,
+                                    limit_price=0.0,
+                                    stop_limit=0.0,
+                                    ticket=position.ticket)
         return None
 
-    def check_close(self, position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager) -> None:
+    def check_close(self, position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager, dataframe: DataFrame) -> None:
+        self._reset_state()
         if not self._is_to_close(position, trade_risk):
             return None
         symbol_attr = api.get_symbol_attributes(position.symbol)
         self._change_state_close(position, symbol_attr)
-        self.notify()
+        self._notify()
 
     def _is_to_close(self, position: Position, trade_risk: TradeRiskManager) -> bool:
         settings = trade_risk.risk_settings
 
-        if position.magic != self.magic or position.timeframe != settings.timeframe:
+        if not self._is_the_right_position(position):
             return False
 
+        # BELOW ALL THE CONDITIONS TO CLOSE THE CURRENT POSITION
+        # THE ORDER DOES NOT MATTER. THE FIRST TRUE CONDITION WILL CLOSE THE POSITION
         if position.profit >= settings.op_goal:
             print(f"{position.symbol} - check close: goal: ${settings.op_goal:.2f} | current profit: ${position.profit}")
             return True
+
         return False
 
+    def _is_the_right_position(self, position: Position) -> bool:
+        return position.magic == self.magic and position.timeframe == self._settings.timeframe
+
     def _change_state_close(self, position: Position, symbol_attributes: Attributes) -> None:
-        # TODO Refactor this position type == 0 to use enum class in comparison
-        if position.type == 0:
-            order_type = OrderType.BUY
-        else:
-            order_type = OrderType.SELL
-
-        symbol = symbol_attributes.symbol
-        timeframe = "NA"
-        action = OrderExecution.CLOSE_POSITION
-        spread = symbol_attributes.spread
-        digits = symbol_attributes.digits
-        ticket = position.ticket
-        self._set_state(symbol, timeframe, action, order_type, 0.0, spread, digits, 0.0, 0.0, 0.0, 0.0, ticket)
-        return None
-
-    def _set_state(self, symbol: str, timeframe: str, action: OrderExecution, order_type: OrderType, price_open: float,
-                   spread: float, digits: int, stop_loss: float, stop_gain: float, limit_price: float,
-                   stop_limit: float, ticket: int) -> None:
-        self._state = StrategyState(symbol=symbol,
-                                    timeframe=timeframe,
+        self._state = StrategyState(symbol=symbol_attributes.symbol,
+                                    timeframe="NA",
                                     strategy=self.name,
                                     magic=self.magic,
-                                    action=action,
+                                    action=OrderExecution.CLOSE_POSITION,
                                     settings=self._settings,
-                                    order_type=order_type,
-                                    price=price_open,
-                                    spread=spread,
-                                    digits=digits,
-                                    stop_loss=stop_loss,
-                                    stop_gain=stop_gain,
-                                    limit_price=limit_price,
-                                    stop_limit=stop_limit,
-                                    ticket=ticket)
+                                    order_type=position.type,
+                                    price=0.0,
+                                    spread=symbol_attributes.spread,
+                                    digits=symbol_attributes.digits,
+                                    stop_loss=0.0,
+                                    stop_gain=0.0,
+                                    limit_price=0.0,
+                                    stop_limit=0.0,
+                                    ticket=position.ticket)
+        return None
--- strategies/strategy.py
+++ strategies/strategy.py
@@ -1,14 +1,25 @@
 from __future__ import annotations
 from abc import abstractmethod
+from dataclasses import dataclass
 from design_patterns.observer_pattern import Subject
 from typing import List, TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from api import Attributes, MarketDataAPI, Position
+    from api import MarketDataAPI, Position, TimeFrames
     from pandas import DataFrame
     from risk_management import TradeRiskManager
     from shared_data_structures import StrategyState, StrategySettings
     from signals import SignalObj
+
+
+@dataclass
+class ProtectState:
+    pass
+
+
+@dataclass
+class CloseState:
+    pass
 
 
 class Strategy(Subject):
@@ -16,12 +27,16 @@ class Strategy(Subject):
         super().__init__()
         self.name = name
         self.magic = magic_number
+        self._is_buy = False
+        self._is_sell = False
         self._is_to_protect_position: List[bool] = []
         self._is_to_close_position: List[bool] = []
         self._state: StrategyState or None = None
+        self._protect_state: ProtectState or None = None
+        self._close_state: CloseState or None = None
         self._settings: StrategySettings or None = None
 
-    def notify(self) -> None:
+    def _notify(self) -> None:
         if self._state is None:
             return None
         for ob in self.observers:
@@ -34,33 +49,24 @@ class Strategy(Subject):
                 return signal
         return None
 
-    def _set_strategy_settings(self, settings: StrategySettings) -> None:
+    def set_strategy_settings(self, settings: StrategySettings) -> None:
         self._settings = settings
 
+    def _reset_state(self) -> None:
+        self._is_buy = False
+        self._is_sell = False
+        self._is_to_protect_position.clear()
+        self._is_to_close_position.clear()
+        self._state = None
+
     @abstractmethod
-    def check_new_position(self, symbol: str, timeframe: str, dataframe: DataFrame, signals: List[SignalObj]) -> None:
+    def check_new_position(self, symbol: str, timeframe: TimeFrames, dataframe: DataFrame, signals: List[SignalObj]) -> None:
         pass
 
     @abstractmethod
-    def check_protect(self, position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager) -> None:
+    def check_protect(self, position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager, dataframe: DataFrame) -> None:
         pass
 
     @abstractmethod
-    def check_close(self,  position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager) -> None:
-        pass
-
-    @abstractmethod
-    def _check_signals(self, symbol: str, timeframe: str, signals: List[SignalObj]) -> None:
-        pass
-
-    @abstractmethod
-    def _change_state_new_position(self, symbol: str, timeframe: str, dataframe: DataFrame) -> None:
-        pass
-
-    @abstractmethod
-    def _change_state_protect(self, position: Position, symbol_attributes: Attributes) -> None:
-        pass
-
-    @abstractmethod
-    def _change_state_close(self, position: Position, symbol_attributes: Attributes) -> None:
+    def check_close(self,  position: Position, api: MarketDataAPI, trade_risk: TradeRiskManager, dataframe: DataFrame) -> None:
         pass
